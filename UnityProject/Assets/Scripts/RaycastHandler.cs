0b466e850fdcc70b5ecc02e558469df7bb315dac
Diego Borges <dborgesrmit@gmail.com>
2019-01-19 20:02:11 -0500

    fixed lookAtCamera

---
+++ UnityProject/Assets/Scripts/RaycastHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHandler : MonoBehaviour
+{
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.tag == "Galaxy")
+            {
+                Debug.Log("Looking at a galaxy");
+            }
+        }
+    }
+}
