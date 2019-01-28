1763f77bca3b38dabebc1351f2b487b3c9d25d7a
Paul Sorenson <paul.sorenson@zenuity.com>
2019-01-28 16:21:43 -0500

    Google Daydream support and new scene for it:

---
+++ UnityProject/Assets/Scripts/FlyAroundDaydreamController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyAroundDaydreamController : MonoBehaviour
+{
+    public GameObject dayDreamCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GvrControllerInput.GetDevice(GvrControllerHand.Dominant).GetButton(GvrControllerButton.TouchPadTouch))
+        {
+            var pointingDirection = dayDreamCamera.transform.forward;
+            transform.Translate(pointingDirection * Time.deltaTime * 5.0f);
+        }
+    }
+}
